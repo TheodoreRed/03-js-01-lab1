beaff41d4de6e7ff872ab42648b69c4bd0996db9
Theodore Redlon <redlontheo@gmail.com>
2023-10-10 15:42:19 -0400

    initial commit

---
+++ challenge.js
@@ -0,0 +1,12 @@
+const calculatePay = (wage, hours) => {
+  if (hours < 40) return wage * hours;
+  let overTimeWage = wage * 1.5;
+  let extra = hours - 40;
+  return 40 * wage + extra * overTimeWage;
+};
+
+const weeksToGoal = (wage, hours, goal) => {
+  return Math.ceil(goal / calculatePay(wage, hours));
+};
+
+console.log(weeksToGoal(10, 50, 1000000));
---
+++ script.js
@@ -0,0 +1,36 @@
+const fullName = "Theodore Redlon";
+const age = 25;
+const birthday = "January 24";
+const pineapplePizza = false;
+const lifeEvents = [
+  "I was born in Marquette, Michigan.",
+  "I went to Grand Circus.",
+  "I participated in basketball, football, and competitive video games.",
+  "I love to be with friends.",
+];
+if (pineapplePizza) {
+  console.log(
+    `My name is ${fullName} and I like pineapples on pizza. I am currently ${age} years old and my birthday is on ${birthday}`
+  );
+} else {
+  console.log(
+    `My name is ${fullName} and I'm not into pineapples on pizza. I am currently ${age} years old and my birthday is on ${birthday}`
+  );
+}
+for (let i = 0; i < lifeEvents.length; i++) {
+  console.log(lifeEvents[i]);
+}
+let counter = 0;
+while (true) {
+  let randomNumber = Math.ceil(Math.random() * 10);
+  if (randomNumber !== 5) {
+    counter++;
+    console.log(`${randomNumber} !== 5`);
+  } else {
+    counter++;
+    console.log(
+      `5 === 5. It took ${counter} iterations to randomly generate the number 5`
+    );
+    break;
+  }
+}
